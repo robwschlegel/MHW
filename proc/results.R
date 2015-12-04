6a366c73a59429876540a77a966891e6af8508e5
schrob040 <wiederweiter@gmail.com>
2015-12-04 18:50:11 +0200

    Added csv files from Eric's analysis.

---
+++ proc/results.R
@@ -0,0 +1,9 @@
+## Extract results of heat wave numbers etc by coast and by time during time series
+
+# Note broad patterns along entire coast
+
+# Create figures showing time of heat waves through time
+
+# Seperate time series by coastal sections
+
+# Extract two largest events per coast for further analysis
